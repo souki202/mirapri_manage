aad60029aacf55508ca424c06bc28e47d0d221f3
souki202 <290Livermorium@gmail.com>
2019-02-10 20:56:50 +0900

    ログインブランチ initial commit

--- app/controllers/public/login_controller.rb
+++ app/controllers/public/login_controller.rb
@@ -2,4 +2,10 @@ class Public::LoginController < ApplicationController
   def index
     @title = 'ログイン'
   end
+
+  def complete
+    user_id = params[:user_ids]
+    password = params[:password]
+    render :index
+  end
 end
